df6972046d68c5c1b83c3bf3b55be4a0d0826ca6
pushpinder <pushp@DESKTOP-3EEE01S>
2018-07-10 17:21:50 -0400

    reverse string code added

--- EmployeeHelpApp/EmployeeClient/Program.cs
+++ EmployeeHelpApp/EmployeeClient/Program.cs
@@ -6,8 +6,14 @@ namespace EmployeeClient
     {
         static void Main(string[] args)
         {
-            Person person = new Person();
-            string info=person.GetPersonInfo("Pushpinder", "Kaur", 55, 123456);
+            Person person;// reference to Person class
+            person= new Person("Meghan","Damminger",32,456987);//instatianting of class object
+            string password = person.GetPassword("test", "123456");
+            Console.WriteLine(password);
+            string fname = Console.ReadLine();
+            string lName = Console.ReadLine();
+            short age = 10;long ssn = 123456;
+            string info=person.GetPersonInfo(fname,lName,age,ssn);
             Console.WriteLine(info);
             Console.Read();
         }
--- EmployeeHelpApp/EmployeeLibrary/Person.cs
+++ EmployeeHelpApp/EmployeeLibrary/Person.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EmployeeLibrary
 {
@@ -11,7 +7,44 @@ namespace EmployeeLibrary
         long ssn;
         short age;
         string firstName, lastName;
-
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public Person()
+        {
+            ssn = 87456;
+            age = 44;
+            firstName = "Carol";
+            lastName = "Baxtor";
+            Console.WriteLine($"Name :{firstName} {lastName}, Age : {age}, SSN : {ssn}");
+        }
+        /// <summary>
+        /// parameterised constructor
+        /// </summary>
+        public Person(string firstName, string lastName, short age, long ssn)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.ssn = ssn;
+            Console.WriteLine($"Name :{firstName} {lastName}, Age : {age}, SSN : {ssn}");
+        }
+        /// <param name="firstName"></param>
+        /// <param name="ssn"></param>
+        /// <returns></returns>
+        public string GetPassword(string firstName, string ssn)
+        {
+            string password="", joinstr;
+            int length;
+            joinstr = firstName + ssn;
+            length = joinstr.Length - 1;
+            while (length>=0)
+            {
+                password = password + joinstr[length];
+                length--;
+            }
+            return password;
+        }
         public string GetPersonInfo(string firstName, string lastName, short age, long ssn)
         {
             return $"Name :{firstName} {lastName}, Age : {age}, SSN : {ssn}";
