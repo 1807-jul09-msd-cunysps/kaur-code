b5abda2156a5ada0a9d93f1447341a4ac2bdc5e0
pushpinder <pushp@DESKTOP-3EEE01S>
2018-07-10 14:46:22 -0400

    test

---
+++ EmployeeHelpApp/EmployeeClient/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using EmployeeLibrary;
+namespace EmployeeClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            string info=person.GetPersonInfo("Pushpinder", "Kaur", 55, 123456);
+            Console.WriteLine(info);
+            Console.Read();
+        }
+    }
+}
---
+++ EmployeeHelpApp/EmployeeLibrary/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeLibrary
+{
+    public class Person
+    {
+        long ssn;
+        short age;
+        string firstName, lastName;
+
+        public string GetPersonInfo(string firstName, string lastName, short age, long ssn)
+        {
+            return $"Name :{firstName} {lastName}, Age : {age}, SSN : {ssn}";
+        }
+    }
+}
