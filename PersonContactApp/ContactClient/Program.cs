f6103797b2193129efb224d894a7a9851dfda51f
pushpinder <pushp@DESKTOP-3EEE01S>
2018-07-12 17:33:14 -0400

    Exception Handling

--- PersonContactApp/ContactClient/Program.cs
+++ PersonContactApp/ContactClient/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,18 +7,75 @@ using System.Threading.Tasks;
 using ContactLibrary;
 namespace ContactClient
 {
+    class DatabaseNotResponding:ApplicationException
+    {
+        public DatabaseNotResponding():base(message)
+        {
+            // logic to exception
+        }
+    }
     class Program
     {
+        static int Result(int a, int b)
+        {
+            try
+            {
+                throw new DatabaseNotResponding();
+            }
+            catch (Exception ex)
+            {
 
+            }
+            var logger = NLog.LogManager.GetCurrentClassLogger();
+            ArrayList result = new ArrayList();
+            result.Add("test 1");
+            result.Add("test 2");
+            result.Add("test 3");
+            result.Add(5);
+            result.Add('a');
+            result.Add(a);
+            result.Add(b);
+            try
+            {
+                foreach (string item in result)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            catch (DivideByZeroException ex)
+            {
+                //Log the exception
+                
+                logger.Info(ex.Message);
+                Console.WriteLine("Please provide non zero denominator");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                //closing resources code here
+                Console.WriteLine("I always make sure to close unmanaged resources");
+            }
+
+            return 0;
+        }
         static void Main(string[] args)
         {
-            Person p = new Person();
+            #region Exception
+            var output = Result(999999999,9);
+            Console.WriteLine(output);
+            #endregion
+
+            #region LINQ
+          /*  Person p = new Person();
             var persons = p.Get();
             //LINQ-Language Integrate Query
             //Query Syntax
             /*var query = from p1 in persons
                         where p1.firstName.StartsWith("T")
-                        select p1;*/
+                        select p1;
            var query = from p1 in persons
                          where p1.address.houseNum.Equals("121")
                          select p1;
@@ -26,11 +84,12 @@ namespace ContactClient
 
             /*var query = from m in marks
                         where m > 60 && m<80
-                        select m;*/
+                        select m;
             foreach (var item in query)
             {
                 Console.WriteLine($"{item.firstName} {item.lastName} Phone +{item.phone.countrycode+"-"+item.phone.areaCode+"-"+item.phone.number}");
-            }
+            }*/
+            #endregion
             //Method Syntax
         }
     }
