761b682c525536c6870033d0123c9cf17a9daf07
pushpinder <pushp@DESKTOP-3EEE01S>
2018-07-18 14:46:37 -0400

    COnsume REST Api

---
+++ ConsumeRestApi/ConsumeRestApi/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+using Models;
+
+namespace ConsumeRestApi
+{
+    class Program
+    {
+        static async Task<string> get()
+        {
+            HttpClient client = new HttpClient();
+           // client.BaseAddress = new Uri("https://jsonplaceholder.typicode.com/users");
+            var response = await client.GetAsync("https://jsonplaceholder.typicode.com/users");
+            var result = response.Content.ReadAsStringAsync().Result;
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            var data = get();
+            Console.WriteLine(data);
+            Console.Read();
+        }
+    }
+}
---
+++ ConsumeRestApi/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class User
+    {
+        public User()
+        {
+            address = new Address();
+            company = new Company();
+        }
+        public int  id { get; set; }
+        public string name { get; set; }
+        public string username { get; set; }
+        public string email { get; set; }
+        public Address address { get; set; }
+        public string phone { get; set; }
+        public string website { get; set; }
+        public Company company { get; set; }
+    }
+}
