911966d880a86943bb57e2eed789fa4701da0c0e
pushpinder <pushp@DESKTOP-3EEE01S>
2018-07-18 15:23:07 -0400

    deserialize the response

--- ConsumeRestApi/ConsumeRestApi/Program.cs
+++ ConsumeRestApi/ConsumeRestApi/Program.cs
@@ -5,23 +5,29 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using System.Net.Http.Headers;
 using Models;
+using Newtonsoft.Json;// library to serialize and deserialize
 
 namespace ConsumeRestApi
 {
     class Program
     {
-        static async Task<string> get()
+        static async Task<IEnumerable<User>> get()
         {
             HttpClient client = new HttpClient();
            // client.BaseAddress = new Uri("https://jsonplaceholder.typicode.com/users");
             var response = await client.GetAsync("https://jsonplaceholder.typicode.com/users");
-            var result = response.Content.ReadAsStringAsync().Result;
-            return result;
+            var result = await response.Content.ReadAsStringAsync();
+            var users= JsonConvert.DeserializeObject<IEnumerable<User>>(result);
+            return users;
         }
         static void Main(string[] args)
         {
-            var data = get();
-            Console.WriteLine(data);
+            var users = get();
+            foreach (var user in users.Result)
+            {
+                Console.WriteLine($"{user.id} {user.username}  {user.phone}");
+            }
+            
             Console.Read();
         }
     }
